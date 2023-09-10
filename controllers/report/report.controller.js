9e800ec4db95b521bbf90734af5ec0d4c0491a06
mhk98 <mohsinkabirseo@gmail.com>
2023-09-11 05:13:58 +0600

    latest update

--- controllers/report/report.controller.js
+++ controllers/report/report.controller.js
@@ -41,6 +41,34 @@ exports.getAllReport = async (req, res) => {
     });
   }
 };
+exports.singleReport = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const report = await Report.findOne({
+      where: { Id: id },
+    });
+
+    if (!report) {
+      return res.status(401).send({
+        status: "fail",
+        message: "No report found",
+      });
+    }
+    res.status(200).send({
+      status: "Success",
+      message: "Successfully got your report",
+      data: report,
+    });
+  } catch (error) {
+    ErrorLogger.error(req.originalUrl + " " + error.message);
+    res.status(500).json({
+      status: "fail",
+      message: "Something went wrong",
+      error: error.message,
+    });
+  }
+};
 exports.deleteReport = async (req, res) => {
   try {
     const { id } = req.params;
--- routes/users/index.js
+++ routes/users/index.js
@@ -6,5 +6,6 @@ router.post("/signup", user.signup);
 router.post("/login", user.login);
 router.post("/refresh-token", user.refreshToken);
 router.get("/", auth("admin"), user.getAllUsers);
+router.get("/:id", auth("admin", "user"), user.getSingleUser);
 
 module.exports = router;
